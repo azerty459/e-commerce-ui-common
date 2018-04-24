2d3152cde582e0332eeaff8f7f06416dc97b09e2
Rémy Halipré <rhalipre@nextoo.fr>
2018-04-24 17:45:26 +0200

    Init

--- src/app/business/produit.business.ts
+++ src/app/business/produit.business.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs/Rx';
 import { Produit } from "../models/Produit";
 import {Injectable} from "@angular/core";
 import {Http, Response} from "@angular/http";
-import {environment} from '../environments/environment'
+import {environment} from '../../environments/environment'
 
 
 @Injectable()
