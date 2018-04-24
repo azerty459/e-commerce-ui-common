567a5b017074a200e9b2cc203ae5a2e07f24f606
Rémy Halipré <rhalipre@nextoo.fr>
2018-04-24 17:52:10 +0200

    Init

--- src/app/e-commerce-ui-common.component.ts
+++ src/app/e-commerce-ui-common.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProduitBusiness} from "./business/produit.business";
 
 @Component({
   selector: 'app-e-commerce-ui-common',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ECommerceUiCommonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private produitBusiness: ProduitBusiness) { }
 
   ngOnInit() {
+
+  }
+
+  getProduit(){
+    this.produitBusiness.getProduit();
   }
 
 }
