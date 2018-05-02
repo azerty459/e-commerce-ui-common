33d61a31b2945139f2eaf5a317a0964ee41e299f
Jérôme Delécluse <jdelecluse@nextoo.fr>
2018-05-02 15:55:43 +0200

    Ajout modèle Categorie

---
+++ models/Categorie.ts
@@ -0,0 +1,11 @@
+export class Categorie {
+
+  constructor(
+    public id: number,
+    public nom: string,
+    public borneGauche: number,
+    public borneDroit: number,
+    public level: number
+  ) {}
+
+}
