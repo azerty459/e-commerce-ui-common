f352406ca1439ac3c3e9c120c60889ade66d1cbd
rantoine <aruffino@nextoo.fr>
2018-06-12 10:02:04 +0200

    Reperation merge git common

--- business/produit.service.ts
+++ business/produit.service.ts
@@ -1,12 +1,15 @@
 import {throwError as observableThrowError} from 'rxjs';
-import { ObservableInput} from 'rxjs/index';
+import {Observable, ObservableInput, Subject} from 'rxjs/index';
 import {Produit} from '../models/Produit';
 import {Injectable} from '@angular/core';
 import {environment} from '../../src/environments/environment';
 import {Pagination} from "../models/Pagination";
-import {Categorie} from "../models/Categorie";
-import {Photo} from "../models/Photo";
-import {HttpClient} from "@angular/common/http";
+import {Categorie} from '../models/Categorie';
+import {Photo} from '../models/Photo';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/observable/of';
+
+
 
 /**
  * Business permettant de gérer les requêtes au niveau de l'api pour l'objet produit.
@@ -15,7 +18,16 @@ import {HttpClient} from "@angular/common/http";
 @Injectable()
 export class ProduitBusiness {
   constructor(private http: HttpClient) {
+
+    // Observable mettant à jour l'observable donnant la liste des produits
+    this.subject = new Subject<Pagination>();
+
   }
+
+  public searchedText: string;
+  public pageNumber: number;
+  public nbProduits: number;
+  public subject: Subject<Pagination>;
 
   /**
    * Retourne une erreur si le business n'a pas pu exécuter le post
@@ -29,13 +41,13 @@ export class ProduitBusiness {
 
   /**
    * Retourne un observable contenant une liste de produit contenu dans la base de données.
-   * @returns {Observable<Produit[]>} Un observable contenant une liste de produit
+   * @returns {Promise<Produit[]>} Un observable contenant une liste de produit
    */
   public getProduit(): Promise<Produit[]> {
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_url, {query: '{ produits {ref nom description prixHT } }'});
     // On créer une promesse
-    let promise = new Promise<Produit[]>((resolve) => {
+    const promise = new Promise<Produit[]>((resolve) => {
       postResult
       // On transforme en promise
         .toPromise()
@@ -51,17 +63,19 @@ export class ProduitBusiness {
     return promise;
   }
 
+
+
   /**
    * Va chercher un produit correspondant à la ref indiqué en paramètre
    * On utilise un map pour récupérer un tableau (arrayCategorie) composé des différentes catégories du produit.
    * @param {string} refProduit La référence du produit recherché
-   * @returns {Observable<Produit>} Le résultat de la recherche du produit.
+   * @returns {Promise<Produit>} Le résultat de la recherche du produit.
    */
   public getProduitByRef(refProduit: String): Promise<any> {
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_url, {query: '{ produits(ref: "' + refProduit + '") {ref nom description prixHT categories{id nom} photos {url} } }'});
     // On créer une promesse
-    let promise = new Promise<any>((resolve) => {
+    const promise = new Promise<any>((resolve) => {
       postResult
       // On transforme en promise
         .toPromise()
@@ -69,7 +83,7 @@ export class ProduitBusiness {
           response => {
             const produits = response['produits'];
             // On résout notre promesse
-            if (response['produits'] == undefined) {
+            if (response['produits'] === undefined) {
               resolve(response[0].message);
             } else {
               const produit = response['produits'][0];
@@ -88,6 +102,50 @@ export class ProduitBusiness {
     return promise;
   }
 
+
+  /**
+   * Communique avec l'API pour aller chercher le texte recherché et selon la pagination demandée
+   * @param {number} page ne n° de page sur laquelle on est
+   * @param {number} nombreDeProduit le nombre de produits à afficher sur la page
+   * @param {string} text le texte recherché
+   * @returns {Promise<Pagination>} une promesse de Pagination
+   */
+  public getProduitByPaginationSearch(page: number, nombreDeProduit: number, text: string): Promise<Pagination> {
+
+    this.searchedText = text;
+
+    const postResult = this.http.post<Pagination>(environment.api_url, {
+      query: '{ pagination(type: "produit", page: ' + page + ', npp: ' + nombreDeProduit + ', nom: "' + this.searchedText +
+      '") { pageActuelle pageMin pageMax total produits { ref nom description prixHT } } }'
+    });
+
+    const promise = new Promise<Pagination> ( (resolve, reject) => {
+
+      postResult.toPromise().then(
+        (response) => {
+          const pagination = response['pagination'];
+          const array = pagination.produits.map((produit) => new Produit(produit.ref, produit.nom, produit.description, produit.prixHT));
+          resolve(new Pagination(pagination.pageActuelle, pagination.pageMin, pagination.pageMax, pagination.total, array));
+        }
+      );
+    });
+
+    return promise;
+  }
+
+  /**
+   * Va chercher les données à afficher selon la recherche donnée en paramètre
+   * @param {string} text le texte recherché
+   * @returns {Promise<void>}
+   */
+  public async search(text: string) {
+
+    const resultat = await this.getProduitByPaginationSearch(this.pageNumber, this.nbProduits, text);
+    this.subject.next(resultat);
+
+  }
+
+
   /**
    * Retourne une page paginée selon les paramètres voulus.
    * @param {number} page La page souhaitant être affichée
@@ -95,27 +153,34 @@ export class ProduitBusiness {
    * @returns {Observable<Pagination>} Un observable contenant un objet pagination
    */
   public getProduitByPagination(page: number, nombreDeProduit: number): Promise<Pagination> {
-    // On récupère l'objet Observable retourné par la requête post
+
+    // Stockage des valeurs de la pagination
+    this.pageNumber = page;
+    this.nbProduits = nombreDeProduit;
+
     const postResult = this.http.post(environment.api_url, {
       query: '{ pagination(type: "produit", page: ' + page + ', npp: ' + nombreDeProduit +
       ') { pageActuelle pageMin pageMax total produits { ref nom description prixHT } } }'
     });
+
     // On créer une promesse
-    let promise = new Promise<Pagination>((resolve) => {
+    const promise = new Promise<Pagination>((resolve) => {
       postResult
       // On transforme en promise
         .toPromise()
         .then(
           response => {
             const pagination = response['pagination'];
-            let array = pagination.produits.map((produit) => new Produit(produit.ref, produit.nom, produit.description, produit.prixHT));
+            const array = pagination.produits.map((produit) => new Produit(produit.ref, produit.nom, produit.description, produit.prixHT));
             resolve(new Pagination(pagination.pageActuelle, pagination.pageMin, pagination.pageMax, pagination.total, array));
           }
         )
         .catch(this.handleError);
     });
     return promise;
+
   }
+
 
   /**
    * Ajoute un produit.
@@ -129,13 +194,13 @@ export class ProduitBusiness {
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_url, {query: 'mutation {addProduit(ref: "' + produit.ref + '", nom: "' + produit.nom + '", description: "' + produit.description + '", prixHT: ' + produit.prixHT + ') { ref nom description prixHT}}'});
     // On créer une promesse
-    let promise = new Promise<any>((resolve) => {
+    const promise = new Promise<any>((resolve) => {
       postResult
       // On transforme en promise
         .toPromise()
         .then(
           response => {
-            if (response['addProduit'] == undefined) {
+            if (response['addProduit'] === undefined) {
               resolve(response);
             } else {
               const produit = response['addProduit'];
@@ -178,7 +243,7 @@ export class ProduitBusiness {
       query: requete
     });
     // On créer une promesse
-    let promise = new Promise<any>((resolve) => {
+    const promise = new Promise<any>((resolve) => {
       postResult
       // On transforme en promise
         .toPromise()
@@ -207,7 +272,7 @@ export class ProduitBusiness {
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_url, {query: 'mutation{deleteProduit(ref: "' + produit.ref + '")}'})
     // On créer une promesse
-    let promise = new Promise<boolean>((resolve) => {
+    const promise = new Promise<boolean>((resolve) => {
       postResult
       // On transforme en promise
         .toPromise()
@@ -234,7 +299,7 @@ export class ProduitBusiness {
       '){ref nom description prixHT categories{id nom} photos {url} }}'
     });
     // On créer une promesse
-    let promise = new Promise<any>((resolve) => {
+    const promise = new Promise<any>((resolve) => {
       postResult
       // On transforme en promesse
         .toPromise()
@@ -267,7 +332,7 @@ export class ProduitBusiness {
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_url, {query: 'mutation{updateProduit(ref:"' + produit.ref + '",supprimerCat:' + categorie.id + ') {ref nom description prixHT categories{id nom} photos {url} } }'});
     // On créer une promesse
-    let promise = new Promise<Produit>((resolve) => {
+    const promise = new Promise<Produit>((resolve) => {
       postResult
       // On transforme en promesse
         .toPromise()
@@ -291,7 +356,7 @@ export class ProduitBusiness {
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_rest_upload_url, dataAEnvoyer);
     // On créer une promesse
-    let promise = new Promise<any>((resolve) => {
+    const promise = new Promise<any>((resolve) => {
       postResult
       // On transforme en promesse
         .toPromise()
