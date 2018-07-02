2121233cfad34344eb6aa0c22220deea922f5a1b
rantoine <aruffino@nextoo.fr>
2018-07-02 10:18:45 +0200

    realisation

--- business/data/categoriedata.service.ts
+++ business/data/categoriedata.service.ts
@@ -2,17 +2,14 @@ import { Injectable } from '@angular/core';
 import { Categorie} from '../../models/Categorie';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { environment } from '../../../src/environments/environment';
-import {throwError as observableThrowError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriedataService {
   public idLastDeletedCategorie: number;
   constructor(private http: HttpClient) { }
-  private handleError (error: HttpErrorResponse | any) {
-    console.error('Categorie Business::handleError', error);
-    return observableThrowError(error);
-  }
+
 
   /**
    * Prend une catégorie en paramètre et retourne la même mais avec le champs "chemin" renseigné
@@ -59,6 +56,7 @@ export class CategoriedataService {
         .toPromise()
         .then(
           response => {
+           console.log(response);
             // On résout notre promesse
             resolve(null);
           }
@@ -71,64 +69,28 @@ export class CategoriedataService {
    * Methode permettant l'envoi de la requéte afin de restaurer la dernière catégorie supprimée
    * @returns {Promise<any>} true si succès false si echec
    */
-  public async restoreLastDeletedCategorie(): Promise<any> {
+  public restoreLastDeletedCategorie(): Promise<number> {
     if (this.idLastDeletedCategorie === undefined){
       this.idLastDeletedCategorie = 0;
     }
     // On récupère l'objet Observable retourné par la requête post
     const postResult = this.http.post(environment.api_url, {
-      query: 'mutation { restoreCategorie(idNouveauParent:' + this.idLastDeletedCategorie + '){id profondeur}}'
+      query: 'mutation { restoreCategorie(idNouveauParent:' + this.idLastDeletedCategorie + ')}'
     });
+    // On créer une promesse
     const promise = new Promise<any>((resolve) => {
       postResult
-      // On transforme en promise
+      // On transforme en promesse
         .toPromise()
         .then(
           response => {
+            console.log(response);
             // On résout notre promesse
-            if (response['restoreCategorie'].length !== 0) {
-              resolve(response['restoreCategorie'][0]);
-            } else {
-              // Pas de categorie
-              console.log('pas de categorie');
-              resolve([]);
-            }
+            resolve(response);
           }
         )
-        .catch(this.handleError);
     });
-    const response = await promise;
-    const profondeur = response.profondeur;
-    const id = response.id;
-    if (profondeur != null && profondeur !== undefined) {
-
-      //  Ici on ecrit la réquéte permettant d'otenir le Json representant l'arbre de categorie avec la bonne
-      //  profondeur.
-      let query = '{ categories(id:' + id + ') { nom id ';
-      for (let i = 0; i < profondeur; i++) {
-        query += 'sousCategories{ nom id ';
-      }
-      for (let i = 0; i < profondeur; i++) {
-        query += '}';
-      }
-      query += '}}';
-
-      // On execute cette requete
-      const postResult = this.http.post(environment.api_url, {query: query});
-      let promise = new Promise<any>((resolve) => {
-        postResult
-        // On transforme en promise
-          .toPromise()
-          .then(
-            response => {
-              // On résout notre promesse et on renvoi l'objet json
-              resolve(response);
-            }
-          )
-          .catch(this.handleError);
-      });
-      return promise;
-    }
+    return promise;
   }
 
 }
