794bc57488bda6a9ce8f15b9d489345197d69c8a
ludovic lahousse <llahousse@nextoo.fr>
2019-05-07 16:15:23 +0200

    amelioration de la promesse getStatistique

--- business/statistique.service.ts
+++ business/statistique.service.ts
@@ -4,6 +4,10 @@ import {environment} from '../../src/environments/environment';
 import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/observable/of';
 import { Statistique } from '../models/Statistique';
+import { resolve } from 'q';
+/*import { catchError } from 'rxjs/internal/operators';
+import { resolve } from 'path';
+import { post } from 'selenium-webdriver/http';*/
 
 /**
  * Business permettant de gérer les requêtes au niveau de l'api pour l'objet statistique.
@@ -27,25 +31,57 @@ export class StatistiqueBusiness {
   /*
    * @returns {Promise<Produit>} Le résultat de la recherche du produit.
    */
-  public getStatistique(): Promise<Statistique> {
-    // On récupère l'objet Observable retourné par la requête post
-    const postResult = this.http.post(environment.api_url, {query: '{nbProduit nbUtilisateur nbCategorie nbProduitCategorie {categorie nb} }'});
-    // On créer une promesse
-    const promise = new Promise<Statistique>((resolve) => {
-      postResult
-      // On transforme en promise
-        .toPromise()
-        .then(
-          response => {
-              console.log(response)
-            const statistique: any = response;
+  async getStatistique(): Promise<Statistique> {
+                                               // Méthode Inception
+
+    // // On récupère l'objet Observable retourné par la requête post
+    // const postResult = this.http.post(environment.api_url, {query: '{nbProduit nbUtilisateur nbCategorie nbProduitCategorie {categorie nb} }'});
+    // // On créer une promesse
+    // const promise = new Promise<Statistique>((resolve) => {
+    //   postResult
+    //   // On transforme en promise
+    //     .toPromise()
+    //     .then(
+    //       response => {
+    //           console.log(response)
+    //         const statistique: any = response;
             
-            // On résout notre promesse
-            resolve(new Statistique(statistique.nbProduit, statistique.nbUtilisateur, statistique.nbCategorie, statistique.nbProduitCategorie));
-          }
-        )
-        .catch(this.handleError);
+    //         // On résout notre promesse
+    //         resolve(new Statistique(statistique.nbProduit, statistique.nbUtilisateur, statistique.nbCategorie, statistique.nbProduitCategorie));
+    //       }
+    //     )
+    //     .catch(this.handleError);
+    // });
+
+    // return postResult;
+
+                                            // Méthode assez efficace 
+
+    // const postResult = this.http.post<Statistique>(environment.api_url, {query: '{nbProduit nbUtilisateur nbCategorie nbProduitCategorie {categorie nb} }'});
+    // return postResult.toPromise();
+
+                                       // Méthode que tu attendais (je pense)
+
+    const postResult = this.http.post(environment.api_url, {query: '{nbProduit nbUtilisateur nbCategorie nbProduitCategorie {categorie nb} }'});
+    let statistique: any;
+
+    await postResult.toPromise().then( 
+    response => {
+      statistique = response;
+    },
+    error => {
+      console.log(error);
+    }).catch(this.handleError);
+
+    let promise =  new Promise<Statistique>((resolve) => {
+      resolve(new Statistique(statistique.nbProduit, statistique.nbUtilisateur, statistique.nbCategorie, statistique.nbProduitCategorie));
     });
+
     return promise;
+    
+      
+      
+    
+   
   }
 }
