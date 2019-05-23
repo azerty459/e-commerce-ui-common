17962e6b3c5c7e23c129b8b1f74859d18c499b69
Arthur Brandao <abrandao@nextoo.fr>
2019-05-23 16:52:49 +0200

    Ajout méthode equals et clone Utilisateur et Role

--- models/Role.ts
+++ models/Role.ts
@@ -5,6 +5,23 @@ export class Role {
     public nom: string,
   ) {
   }
+
+  public static clone(role: Role): Role {
+    return new Role(role.id, role.nom);
+  }
+
+  public static equals(role1: Role, role2: Role): boolean {
+    if (role1 === role2) {
+      return true;
+    }
+    if (role1 === undefined || role2 === undefined) {
+      return false;
+    }
+    let retour = role1.id === role2.id;
+    retour = retour && role1.nom === role2.nom;
+    return retour;
+  }
+
 }
 
 
